e081684a894ca3785769167ac912e5423aa11845
lucas <lucasmartinscaet@hotmail.com>
2023-09-08 22:17:18 -0300

    feat: criação do admin apenas para testes

--- app/markets/admin.py
+++ app/markets/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Market, Product, ProductImage
 
-# Register your models here.
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    extra = 0
+
+
+class ProductInline(admin.StackedInline):
+    model = Product
+    extra = 0
+
+
+@admin.register(Market)
+class MarketAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+    
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]    
