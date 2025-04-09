d8749326476c33dd7e1991d640500ec7e1405f3e
lucas <lucasmartinscaet@hotmail.com>
2025-04-09 20:35:40 -0300

    feat: creating a action to show only promotions
    fix: exclude atribute in pre commit file

--- features/stores/viewsets.py
+++ features/stores/viewsets.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from django.db.models import F, OuterRef, Subquery
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.filters import ParameterizedFilterBackend, SearchFilter
 from features.stores import models, serializers
@@ -6,19 +9,28 @@ from features.stores import models, serializers
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.ProductSerializer
-    queryset = models.Product.objects.all()
+    queryset = models.Product.objects.select_related("category").all()
     permission_classes = []
     filter_backends = [ParameterizedFilterBackend, SearchFilter]
     filter_params_query = {
         "category": "category",
     }
-    
     search_fields = ["name"]
-    
-    # def get_queryset(self):
-    #     return models.Product.objects.all().distinct("name")
+
+    @action(detail=False, methods=["get"])
+    def promotions(self, request):
+        queryset = self.queryset.annotate(
+            lowest_price=Subquery(
+                self.queryset.filter(name=OuterRef("name")).values("price")[:1]
+            )
+        ).filter(price=F("lowest_price"))
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = serializers.CategorySerializer 
+    serializer_class = serializers.CategorySerializer
     queryset = models.Category.objects.all()
     permission_classes = []
