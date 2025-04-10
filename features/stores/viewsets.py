d335a77964bcc105dcc7f9528ae4e59b97010758
lucas <lucasmartinscaet@hotmail.com>
2025-04-10 18:44:39 -0300

    feat: swagger for category

--- features/stores/viewsets.py
+++ features/stores/viewsets.py
@@ -1,4 +1,7 @@
 from django.db.models import F, OuterRef, Subquery
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (OpenApiParameter, extend_schema,
+                                   extend_schema_view)
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -30,6 +33,26 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="width", type=OpenApiTypes.STR, description="Largura do SVG"
+            ),
+            OpenApiParameter(
+                name="height", type=OpenApiTypes.STR, description="Altura do SVG"
+            ),
+            OpenApiParameter(
+                name="fill_svg", type=OpenApiTypes.STR, description="Cor do SVG"
+            ),
+            OpenApiParameter(
+                name="fill_path",
+                type=OpenApiTypes.STR,
+                description="Cor do path do SVG",
+            ),
+        ]
+    )
+)
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.CategorySerializer
     queryset = models.Category.objects.all()
