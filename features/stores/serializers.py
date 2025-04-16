b90abea97e9439afc692090d86079378b778be15
lucas <lucasmartinscaet@hotmail.com>
2025-04-16 17:33:20 -0300

    feat: adding category serializer into ProductSerializer

--- features/stores/serializers.py
+++ features/stores/serializers.py
@@ -3,15 +3,17 @@ from rest_framework import serializers
 from . import models
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Product
-        fields = "__all__"
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = models.Product
         fields = "__all__"
 
 
