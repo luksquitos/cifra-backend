2a8c10ca17af0b4e3d6e476e61b1d674ca4fec32
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-05-18 17:13:27 -0300

    test: trying add Editar Perfil to admin userlinks

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import environ
@@ -66,7 +67,9 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        # "DIRS": [],
+        # 'DIRS': [BASE_DIR / "templates"],
+        # "DIRS": [os.path.join(BASE_DIR, "templates/")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
